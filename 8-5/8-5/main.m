303d50b3cae5182c06315b7a9a73b05d28bd62c7
Keith <371672704@qq.com>
2015-06-26 09:03:40 +0800

    .DS_Store ,never mind

---
+++ 8-5/8-5/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  8-5
+//
+//  Created by Keith on 15-6-25.
+//  Copyright (c) 2015年 Keith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
